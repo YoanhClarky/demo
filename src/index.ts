6532d4a59a071586810b5c334cdd4103c26c30c8
YoanhClarky <manteleyoanh@gmail.com>
2024-07-18 14:27:45 +0200

    insertion cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import * as express from "express";
 import { AppDataSource } from "./data-source";
 import router from "./routes";
+import { User } from "./entity/User";
+const cors = require('cors')
+
 
 const bodyParser = require("body-parser");
 const server = express();
@@ -10,6 +13,13 @@ server.use(express.json());
 server.get("/", (req, res) => {
   res.send("Api en marche");
 });
+server.use(cors())
+
+server.get('/bonjour', async (req, res)=>{
+  const users = await User.find()
+  res.send({message:"Bonjour voici la liste des users", data:users})
+})
+
 
 AppDataSource.initialize()
   .then(async () => {
